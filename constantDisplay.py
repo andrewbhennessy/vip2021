e2c2384214aa7f923c004c2737e0c0a643ba65ba
Andrew Hennessy <andrew@andrewhennessy.net>
2021-04-14 01:00:10 -0400

    Added Display Code

--- constantDisplay.py
+++ constantDisplay.py
@@ -3,8 +3,38 @@ import numpy as np
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
 import time
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
 
 def engage_radar():
+    i2c = busio.I2C(SCL, SDA)
+
+    disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
+    disp.fill(0)
+    disp.show()
+
+    width = disp.width
+    height = disp.height
+    image = Image.new("1", (width, height))
+
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+    padding = -2
+    top = padding
+    bottom = height - padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = 0
+
+    font = ImageFont.load_default()
+
     my_filter = KalmanFilter(dim_x=2, dim_z=1)
 
     my_filter.x = np.array([[2.],
@@ -32,6 +62,7 @@ def engage_radar():
     times = []
     currentTime = int(np.round(time.time() % 60))
     while 1:
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
         while ser.inWaiting() >= 14:
             for elem in range(13):
                 #try:
@@ -51,6 +82,9 @@ def engage_radar():
                     # do something with the output
                     x = my_filter.x
                     print(x[0][0])
+                    draw.text((x, top + 0), "Distance(cm): " + x[0][0], font=font, fill=255)
+                    disp.image(image)
+                    disp.show()
 
 
 
